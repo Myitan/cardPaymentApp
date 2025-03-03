cc26d61d318604488d0f2e3fd2dd50f3d3c80b89
Myitan <matkapitulaa@gmail.com>
2025-03-03 01:59:01 +0100

    Fix railway monorepo deployment

--- backend/src/main/java/com/backend/cardPayment/CardPaymentApplication.java
+++ backend/src/main/java/com/backend/cardPayment/CardPaymentApplication.java
@@ -1,13 +1,25 @@
 package com.backend.cardPayment;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class CardPaymentApplication {
+public class CardPaymentApplication implements WebMvcConfigurer {
+
+	@Value("${allowed.origins}")
+	private String[] allowedOrigins;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardPaymentApplication.class, args);
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins(allowedOrigins)
+				.allowedMethods("*");
+	}
 }
